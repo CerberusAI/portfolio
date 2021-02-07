c15cb18b24d326ba84fe22e2a67410e5f30a5b61
CerberusAI <59752667+CerberusAI@users.noreply.github.com>
2021-02-07 07:40:37 -0500

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,20 @@
+import json
+
+# Entry point function
+def lambda_handler(event, context):
+    
+    # Logs to CloudWatch
+    print(json.dumps(event['body']))
+    
+    # Response JSON sent back client
+    response = {
+        'statusCode': 202,
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST'
+        },
+        'body': 'Roger Roger...'
+    }
+    
+    return response
