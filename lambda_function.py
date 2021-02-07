63c7afce0e84eda09e9342916155cdc9d8161ba5
CerberusAI <59752667+CerberusAI@users.noreply.github.com>
2021-02-07 08:53:22 -0500

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,10 +1,16 @@
 import json
+import boto3    # AWS services
+
+# Homework: develop function to send email and / or text using SNS
+def send_to_SNS(Event):
+    # Logs to CloudWatch
+    print(json.dumps(Event))
 
 # Entry point function
 def lambda_handler(event, context):
-    
-    # Logs to CloudWatch
-    print(json.dumps(event['body']))
+
+    # Process event & send to SNS
+    send_to_SNS(event)
     
     # Response JSON sent back client
     response = {
